99e4b6b0a6b2ce535d8c71e58d51ae96f27603a4
vctrklndr <victor@nasa.se>
2023-06-04 19:43:22 +0200

    Add as heading

--- src/Data/MainData.js
+++ src/Data/MainData.js
@@ -1,7 +1,7 @@
 const MainData = {
   preTitle: "Stina Brage presenterar",
   title: "Bragehain",
-  text: "   <p>Shakesperefabriken</p><p>26 augusti</p>"
+  text: "   <h2>Shakesperefabriken<br />26 augusti</h2>"
 };
 
 export default MainData;
