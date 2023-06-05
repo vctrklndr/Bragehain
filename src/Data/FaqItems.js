aedd873948c7ee3c95be11c2e24481d0f207f3bf
vctrklndr <victor@nasa.se>
2023-06-05 11:54:23 +0200

    Add faq item

--- src/Data/FaqItems.js
+++ src/Data/FaqItems.js
@@ -8,23 +8,28 @@ const FaqItems = {
     },
     {
       id: "2",
+      question: "Hur tar jag mig hem?",
+      answer: "<p>Från festen får en ta sig för egen maskin. Med en 3,4 kilometers cykeltur eller promenad tar man sig enkelt in till Vadstena. Har man tur kan man lyckas boka en taxi eller så kanske man känner någon vänlig själ som kan agera skjuts.</p><p><a href='https://taxibil.se/boka-taxi-i-vadstena/'>Boka taxi</a><br /><a href='https://www.google.se/maps/dir/Shakespearefabriken,+ULLEVI+101,+592+91+Vadstena/Vadstena/@58.4392945,14.8657717,15z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x465bc58a77962821:0x8e40cc502e592179!2m2!1d14.8539094!2d58.4348072!1m5!1m1!1s0x465bc49890157a15:0xbdbd099e87485cd2!2m2!1d14.8902335!2d58.4476022?entry=ttu'>Vägbeskrivning till Vadstena</a></p>"
+    },
+    {
+      id: "3",
       question: "Hur anmäler jag mig?",
       answer:
         "<p>För att anmäla din närvaro OSA till <a href='tel:0733968622'>073–396 86 22</a> senast 20/7.</p>"
     },
     {
-      id: "3",
+      id: "4",
       question: "Kan jag ta med mig mitt barn?",
       answer: "<p>Barn är trevligt, men Bragehain är en barnfri klubb.</p>"
     },
     {
-      id: "4",
+      id: "5",
       question: "Vad gör jag om jag blir hungrig?",
       answer:
         "<p>Det kommer finnas en foodtruck på området som serverar mat under kvällen. Vid eventuella allergier / specialkost ska detta meddelas i och med OSA.</p>"
     },
     {
-      id: "5",
+      id: "6",
       question: "När slutar festligheterna?",
       answer: "<p>Vi måste ha utrymt lokalen 27/8 20.00.</p>"
     }
